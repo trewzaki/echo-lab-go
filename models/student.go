b8143dfba1f38f02698f0acd333a44fe2b48029e
Tananut Panyagosa <tongtananut@gmail.com>
2021-05-27 14:18:05 +0700

    Fix bug and Edit README.md

--- models/student.go
+++ models/student.go
@@ -12,7 +12,6 @@ type StudentScore struct {
 	Name      string         `gorm:"type:varchar(100);not null" json:"name"`
 	Subject   string         `gorm:"type:varchar(100);not null" json:"subject"`
 	Score     float64        `json:"score"`
-	CreatedAt time.Time      `gorm:"DEFAULT:now()" json:"created_at"`
-	UpdatedAt *time.Time     `gorm:"DEFAULT:now()" json:"updated_at"`
+	CreatedAt time.Time      `json:"created_at"`
 	DeletedAt gorm.DeletedAt `gorm:"DEFAULT:NULL" json:"deleted_at"`
 }
